652568cd1fd3bd05111cf3d052465f66201287f9
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-20 10:56:05 -0300

    Fix error

--- src/app/api/checkout-session/route.ts
+++ src/app/api/checkout-session/route.ts
@@ -5,9 +5,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
   throw new Error('STRIPE_SECRET_KEY is not set');
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req: Request) {
   try {
