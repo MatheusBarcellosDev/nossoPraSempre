111c35a28b829fe87da9c67078bbb55d93752f32
MatheusBarcellosDev <matheusbarcellos61@gmail.com>
2025-01-20 10:37:28 -0300

    Fix error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['next-cloudinary'],
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
